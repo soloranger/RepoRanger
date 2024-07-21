9a3903e6c4e9ef951e0a0afb77aff0c6cdc09efb
Mahdi Shadi <mahdishadi99@gmail.com>
2024-07-21 09:54:25 +0330

    Create API Structure

--- api/api/api.py
+++ api/api/api.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
+from .view.v1 import api_bp as apiv1_bp
+
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(apiv1_bp)
     return app
--- api/view/v1/v1.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint 
-from flask_restful import Api
-
-
-apiv1_bp = Blueprint("apiv1_bp",__name__,url_prefix="/api/v1")
-apiv1 = Api(apiv1_bp)
