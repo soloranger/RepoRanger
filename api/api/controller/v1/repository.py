2f110c9f1e798dbab6d97d895f3fb018b7f6df77
Mahdi Shadi <mahdishadi99@gmail.com>
2024-07-28 23:37:56 +0330

    Create V1 of Controller

---
+++ api/api/controller/v1/repository.py
@@ -0,0 +1,15 @@
+class RepositoryController:
+    def get_repositories():
+        return {"ping": "pong"}
+    
+    def get_repository(repository_id):
+        return {"ping": "pong"}
+    
+    def create_repository():
+        return {"ping": "pong"}
+    
+    def update_repository(repository_id):
+        return {"ping": "pong"}
+    
+    def delete_repository(repository_id):
+        return {"ping": "pong"}
--- api/api/view/v1/repository.py
+++ api/api/view/v1/repository.py
@@ -1,15 +1,26 @@
 from flask_restful import Resource
-
+from api.controller.v1 import RepositoryController
 
 class RepositoryResource(Resource):
     def get(self, repository_id=None):
-        return {"ping": "pong"}
+        # GET /api/v1/repositories -> Repository List.
+        # GET /api/v1/repositories/<repository_id> -> Get Repository
+        if repository_id is None:
+            return RepositoryController.get_repositories()
+        else:
+            return RepositoryController.get_repository(repository_id)
 
     def post(self):
-        return {"ping": "pong"}
+        # POST /api/v1/repositories -> Create New Repository .
+        # POST /api/v1/repositories/<repository_id> -> Not Allowd
+        return RepositoryController.create_repository()
 
     def patch(self, repository_id):
-        return {"ping": "pong"}
+        # PATCH /api/v1/repositories -> Not Allowd 
+        # PATCH /api/v1/repositories/<repository_id> -> Update an existing repository
+        return RepositoryController.update_repository(repository_id)
 
     def delete(self, repository_id):
-        return {"ping": "pong"}
+        # DELETE /api/v1/repositories -> Not Allowd 
+        # DELETE /api/v1/repositories/<repository_id> -> Delete  an existing repository        
+        return RepositoryController.delete_repository(repository_id)
