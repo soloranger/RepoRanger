eb1af96d294dfcf336cd578154b38a701fcecb72
Mahdi Shadi <mahdishadi99@gmail.com>
2024-08-12 23:51:44 +0330

    Update Get Repository Func in Controller

--- api/api/controller/v1/repository.py
+++ api/api/controller/v1/repository.py
@@ -1,27 +1,54 @@
 from api.model import Repository
 from api.schema.v1 import RepositorySchema
 from api.util import jsonify
-
+from flask import request
+from api.object import db
 
 class RepositoryController:
     def get_repositories():
         try:
             repositories = Repository.query.all()
         except: 
-                
-        repositories_schema = RepositorySchema(many=True)
-        return jsonify(
-            {
-                 "output" : repositories_schema.dump(repositories),
-            }
-
-            )
+            return jsonify(status=500, code=102)
+        try:
+            repositories_schema = RepositorySchema(many=True)
+        except:
+            return jsonify(status=500, code=103)
+        return jsonify(repositories_schema.dump(repositories))
 
     def get_repository(repository_id):
-        return jsonify(status=501, code=101)
+        try:
+            repository = Repository.query.get(repository_id)
+        except:
+            return jsonify(status=500, code=102)    
+        if  repository is None:
+            return jsonify(status=404, code=105)
+        try:
+            repository_schema = RepositorySchema()
+        except:
+            return jsonify(status=500, code=103)
+        
+        return jsonify(repository_schema.dump(repository))
 
     def create_repository():
-        return jsonify(status=501, code=101)
+        try:
+            repository_schema = RepositorySchema()
+        except:
+            return jsonify(status=500, code=103)    
+        try:
+         request_data = repository_schema.load(request.get_json())
+        except:
+            return jsonify(status=400, code=104)
+             
+        repository = Repository(
+            owner="TBC",
+            url=request_data["url"]
+        )
+        db.session.add(repository)
+        db.session.commit()
+        return jsonify(repository_schema.dump(repository), status=201)
+    
+         
 
     def update_repository(repository_id):
         return jsonify(status=501, code=101)
--- api/api/util/jsonify.py
+++ api/api/util/jsonify.py
@@ -3,14 +3,18 @@ from flask import current_app
 APP_CODES = {
     100: "OK.",
     101: "Feature is not implemented.",
-    102
+    102: "Database error",
+    103: "Schema instance error",
+    104: "Input Validation error",
+    105:  "Resource Not Found",
 }
 
 
 def jsonify(state={}, metadata={}, status=200, code=100, headers={}):
-    resource = state.copy()
-    resource.update(metadata)
-    resource["code"] = code
+    resource = {}
+    resource["output"] = state
+    resource["metadata"] = metadata
+    resource["status"] = {"code": code}
     if current_app.debug is True:
-        resource["message"] = APP_CODES[code]
+        resource["status"]["message"] = APP_CODES[code]
     return resource, status, headers
