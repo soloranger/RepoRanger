5057ecad3b52c099018a65e43533455c89711630
Mahdi Shadi <mahdishadi99@gmail.com>
2024-08-12 13:26:15 +0330

    Add Repository DataBse Schema

---
+++ api/api/schema/v1/repository.py
@@ -0,0 +1,16 @@
+from api.model import Repository
+from api.object import ma
+
+
+class RepositorySchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Repository
+
+    id = ma.auto_field(dump_only=True)  # Just Can see it and Cant modify it
+    owner = ma.auto_field(dump_only=True)
+    url = (
+        ma.auto_field()
+    )  # if we dont set anything => also can see it and can Modify it
+    created_at = ma.auto_field(dump_only=True)
+    last_updated_at = ma.auto_field(dump_only=True)
+    status = ma.auto_field()
