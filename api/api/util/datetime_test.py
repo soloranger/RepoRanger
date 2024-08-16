79d02a5ba6450a8cd042dd9a5531ed4e7c24b780
Mahdi Shadi <mahdishadi99@gmail.com>
2024-08-16 12:12:21 +0330

    Create datetime test Func

--- api/api/util/datetime_test.py
+++ api/api/util/datetime_test.py
@@ -1,7 +1,17 @@
 import pytest
 from api.config import Config
 from .datetime import now
+from datetime import datetime
+from pytz import timezone
 
 def test_now():
-    result = now()
+    app_result = now()
+    sys_result = datetime.now(tz=timezone(Config.TIMEZONE))
+    assert app_result.tzinfo.zone == Config.TIMEZONE
+    assert app_result.year == sys_result.year
+    assert app_result.month == sys_result.month
+    assert app_result.day == sys_result.day
+    assert app_result.hour == sys_result.hour
+    
+
     
